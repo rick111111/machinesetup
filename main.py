c296d0b5102894accf29599562805e91361dab1b
Fangliang <xuefangliang@gmail.com>
2019-07-05 23:24:51 -0700

    list all the files under static

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import os.path
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    with open("./static/winsetup.txt", 'r') as file:
-        return file.read()
+    ret = '<ul>'
+    for name in os.listdir('./static/'):
+        ret += f'<li><a href="./static/{name}">{name}</a></li>'
+    ret += '</ul>'
+    return ret
 
 @app.route('/user/<name>')
 def user(name):
